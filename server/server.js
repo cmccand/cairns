10831b94c58597b3b5c68f57c266fa84af654d57
Chris McCandlish <cmccand@gmail.com>
2024-02-13 23:48:05 -0500

    clean up unused routes

--- server/server.js
+++ server/server.js
@@ -1,37 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const app = express();
-// const path = require('path')
-// const cors = require('cors');
-// const PORT = process.env.PORT || 3000;
-
-// console.log("this is the  server port:", PORT);
-
-// app.use(cors());
-// app.use(express.json());
-
-// // Serve static files from the public directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Set Content-Type header for CSS files
-// app.use('*.css', (req, res, next) => {
-//   res.setHeader('Content-Type', 'text/css');
-//   next();
-// });
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/index.html"))
-// });
-
-// app.get('/activities', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/activities.html'));
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -47,10 +13,6 @@ app.get('/', function(req, res) {
 app.get('/activities', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/activities.html'));
 });
-
-// app.get('/styles', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/styles.css'));
-// });
 
 // Start the server
 app.listen(PORT, () => {
