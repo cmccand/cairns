0b3d1495db475d482dad6c6e78459530d5da5f50
Chris McCandlish <cmccand@gmail.com>
2024-02-13 23:40:09 -0500

    create basic node app

---
+++ public/activities.js
@@ -0,0 +1,5 @@
+import {
+  testFunc
+} from './main.js';
+
+testFunc();
---
+++ public/main.js
@@ -0,0 +1,6 @@
+// public/main.js
+console.log("Main script loaded.");
+
+export function testFunc() {
+  console.log('This is my function');
+}
